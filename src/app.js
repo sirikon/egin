237be584ed516c4fe663ce31415d0ab0c3cc5057
Carlos Fdez. Llamas <hello@sirikon.me>
2020-03-27 22:43:08 +0100

    gjs out

--- src/app.js
+++
@@ -1,7 +0,0 @@
-const {Gtk} = imports.gi;
-const {Window} = imports.components.window;
-
-async function app() {
-    const window = new Window();
-    window.render();
-}
--- src/main.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env gjs
-imports.searchPath.unshift('./src');
-imports.gi.versions.Gtk = '3.0';
-const {Gtk} = imports.gi;
-const {app} = imports.app;
-Gtk.init(null);
-
-app().then(() => {}, (err) => print(err));
-Gtk.main();
