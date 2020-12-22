bbd4761df80baa7eda33d879d087f2e2aa94472a
Carlos Fdez. Llamas <hello@sirikon.me>
2020-12-22 18:53:44 +0100

    cleaner

--- src/js/app.js
+++ src/js/app.js
@@ -1,17 +1,21 @@
 var root = document.getElementById('app')
 var count = 0
 
-var Hello = {
-  view: function() {
-    return m("main", [
-      m("h1", {
-        class: "title"
-      }, "My first app"),
-      m("button", {
-        onclick: function() {count++}
-      }, count + " clicks")
-    ])
-  }
+function Hello() {
+    let count = 0;
+
+    const buttonClick = () => count++;
+    const buttonText = () =>
+        `${count} click${(count !== 1 ? 's' : '')}`;
+
+    function view() {
+        return [
+            m("h1", {class: "title"}, "My first app"),
+            m("button", { onclick: buttonClick }, buttonText())
+        ]
+    }
+
+    return { view }
 }
 
 m.mount(root, Hello)
