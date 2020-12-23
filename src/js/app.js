88d4c1e3b0be25f2fba919c2d73e5492beebec87
Carlos Fdez. Llamas <hello@sirikon.me>
2020-12-23 14:50:31 +0100

    wip

--- src/js/app.js
+++ src/js/app.js
@@ -2,41 +2,80 @@ var root = document.getElementById('app')
 var count = 0
 
 const tasks = [
-    {id: 1, name: 'Easy task boi', done: false},
-    {id: 2, name: 'Harder task boi', done: true}
+    {name: 'Easy task boi', done: false},
+    {name: 'Harder task boi', done: true}
 ]
+
+const uiState = {
+    selectedTaskKey: null
+}
 
 function Task(vnode) {
     const task = () => vnode.attrs.task
+    const isSelected = () => vnode.attrs.key === uiState.selectedTaskKey
+    const classes = () => buildClasses({
+        'is-selected': isSelected()
+    })
 
-    const view = () => m('div.egin-task', [
+    const setSelected = () => uiState.selectedTaskKey = vnode.attrs.key
+    const removeSelected = () => isSelected() && (uiState.selectedTaskKey = null)
+
+    const view = () => m('div.egin-task', {class: classes()}, [
         m('input', {
             type: 'checkbox',
             checked: task().done,
-            onchange: (e) => task().done = e.target.checked
+            onchange: (e) => task().done = e.target.checked,
+            onfocus: setSelected,
+            onblur: removeSelected
         }),
         m('input', {
             type: 'text',
             value: task().name,
-            oninput: (e) => task().name = e.target.value
+            oninput: (e) => task().name = e.target.value,
+            onfocus: setSelected,
+            onblur: removeSelected
         })
     ])
 
     return { view }
 }
 
-function Main() {
-    // let count = 0;
-
-    // const buttonClick = () => count++;
-    // const buttonText = () =>
-    //     `${count} click${(count !== 1 ? 's' : '')}`;
-
+function App() {
     const view = () => [
-        m('div.egin-task-list', tasks.map(t => m(Task, {key: t.id, task: t})))
+        m('div.egin-task-list', tasks.map((task, i) => m(Task, {key: i, task})))
     ]
 
     return { view }
 }
 
-m.mount(root, Main)
+function buildClasses(obj) {
+    return Object.keys(obj).filter(k => obj[k]).join(' ')
+}
+
+const globalKeyHandlers = {
+    ArrowUp: () => {
+        if (uiState.selectedTaskKey === null) { uiState.selectedTaskKey = 0 }
+        uiState.selectedTaskKey = uiState.selectedTaskKey > 0
+            ? uiState.selectedTaskKey - 1
+            : 0
+    },
+    ArrowDown: () => {
+        if (uiState.selectedTaskKey === null) { uiState.selectedTaskKey = 0 }
+        uiState.selectedTaskKey = uiState.selectedTaskKey < tasks.length-1
+            ? uiState.selectedTaskKey + 1
+            : tasks.length-1
+    },
+    Escape: () => uiState.selectedTaskKey = null
+}
+
+document.addEventListener('keydown', (e) => {
+    if (globalKeyHandlers[e.key]) {
+        e.preventDefault()
+        e.stopPropagation()
+        globalKeyHandlers[e.key](e)
+        m.redraw.sync()
+        document.querySelector('.egin-task.is-selected input[type="text"]').focus()
+    }
+});
+
+m.mount(root, App)
