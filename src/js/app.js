90c25cc6081a93cc8c91557238a7d8b1afa48588
Carlos Fdez. Llamas <hello@sirikon.me>
2020-12-23 12:45:16 +0100

    wip

--- src/js/app.js
+++ src/js/app.js
@@ -1,21 +1,42 @@
 var root = document.getElementById('app')
 var count = 0
 
-function Hello() {
-    let count = 0;
+const tasks = [
+    {id: 1, name: 'Easy task boi', done: false},
+    {id: 2, name: 'Harder task boi', done: true}
+]
 
-    const buttonClick = () => count++;
-    const buttonText = () =>
-        `${count} click${(count !== 1 ? 's' : '')}`;
+function Task(vnode) {
+    const task = () => vnode.attrs.task
 
-    function view() {
-        return [
-            m("h1", {class: "title"}, "My first app"),
-            m("button", { onclick: buttonClick }, buttonText())
-        ]
-    }
+    const view = () => m('div.egin-task', [
+        m('input', {
+            type: 'checkbox',
+            checked: task().done,
+            onchange: (e) => task().done = e.target.checked
+        }),
+        m('input', {
+            type: 'text',
+            value: task().name,
+            oninput: (e) => task().name = e.target.value
+        })
+    ])
 
     return { view }
 }
 
-m.mount(root, Hello)
+function Main() {
+    // let count = 0;
+
+    // const buttonClick = () => count++;
+    // const buttonText = () =>
+    //     `${count} click${(count !== 1 ? 's' : '')}`;
+
+    const view = () => [
+        m('div.egin-task-list', tasks.map(t => m(Task, {key: t.id, task: t})))
+    ]
+
+    return { view }
+}
+
+m.mount(root, Main)
