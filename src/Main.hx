67a012230dc9828056f6995558786f3a274cc804
Carlos Fdez. Llamas <hello@sirikon.me>
2021-01-03 14:41:30 +0100

    wip

--- src/Main.hx
+++ src/Main.hx
@@ -1,10 +1,11 @@
 import js.Browser;
 class Main {
     static function main() {
+        var count = 0;
         var button = Browser.document.createButtonElement();
         button.textContent = "Click me!";
         button.onclick = function(event) {
-            Browser.alert("Haxe is osom");
+            Browser.alert("Haxe is " + count++);
         }
         Browser.document.body.appendChild(button);
     }
