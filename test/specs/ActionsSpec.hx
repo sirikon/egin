df7e8a5fec7c8b59b26bf2deee512749b23c598b
Carlos Fdez. Llamas <hello@sirikon.me>
2021-01-05 17:35:09 +0100

    wip

--- test/specs/ActionsSpec.hx
+++ test/specs/ActionsSpec.hx
@@ -40,4 +40,92 @@ class ActionsSpec {
         actions.setSelectedTaskIndex(2);
         expect(actions.getSelectedTaskIndex()).toEqual(2);
     }
+
+    public function toggle_selected_task_should_not_do_anything_with_no_task_selected() {
+        utils.givenTasks([
+            task('0', false),
+            task('1', false),
+        ]);
+        actions.toggleSelectedTask();
+        utils.expectTasks([
+            task('0', false),
+            task('1', false),
+        ]);
+    }
+
+    public function toggle_selected_task_should_enable_and_disable_selected_task() {
+        utils.givenTasks([
+            task('0', false),
+            task('1', false),
+            task('2', false),
+        ]);
+        actions.setSelectedTaskIndex(1);
+        actions.toggleSelectedTask();
+        utils.expectTasks([
+            task('0', false),
+            task('1', true),
+            task('2', false),
+        ]);
+        actions.toggleSelectedTask();
+        utils.expectTasks([
+            task('0', false),
+            task('1', false),
+            task('2', false),
+        ]);
+    }
+
+    public function remove_task_should_remove_given_task() {
+        utils.givenTasks([
+            task('0', false),
+            task('1', false),
+            task('2', false),
+        ]);
+        actions.setSelectedTaskIndex(1);
+        actions.removeTask(2);
+        utils.expectTasks([
+            task('0', false),
+            task('1', false),
+        ]);
+        expect(actions.getSelectedTaskIndex()).toEqual(1);
+    }
+
+    public function remove_task_should_remove_given_task_and_change_selected_task_index() {
+        utils.givenTasks([
+            task('0', false),
+            task('1', false),
+            task('2', false),
+        ]);
+        actions.setSelectedTaskIndex(2);
+        actions.removeTask(1);
+        utils.expectTasks([
+            task('0', false),
+            task('2', false),
+        ]);
+        expect(actions.getSelectedTaskIndex()).toEqual(1);
+    }
+
+    public function remove_task_should_remove_given_task_and_its_subtasks() {
+        utils.givenTasks([
+            task('0', false),
+            task('1', false, [
+                task('2', false),
+                task('3', false, [
+                    task('4', false),
+                ]),
+                task('5', false),
+            ]),
+            task('6', false, [
+                task('7', false),
+            ]),
+        ]);
+        actions.setSelectedTaskIndex(6);
+        actions.removeTask(1);
+        utils.expectTasks([
+            task('0', false),
+            task('6', false, [
+                task('7', false),
+            ]),
+        ]);
+        expect(actions.getSelectedTaskIndex()).toEqual(1);
+    }
 }
