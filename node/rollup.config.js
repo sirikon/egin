c3d3b893b3c1cc19cebb067ffa7df1887e17af31
Carlos Fdez. Llamas <hello@sirikon.me>
2021-01-10 23:55:28 +0100

    wip

--- node/rollup.config.js
+++ node/rollup.config.js
@@ -1,9 +1,9 @@
-import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
+import { nodeResolve } from '@rollup/plugin-node-resolve';
 import { terser } from "rollup-plugin-terser";
 
 export default {
-    input: 'libs.js',
+    input: '../src/libs.js',
     output: {
         name: 'eginlibs',
         file: '../dist/libs.js',
