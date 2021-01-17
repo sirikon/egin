f576a4f83d597ac03ff622e8c8b1d6511ec357a9
Carlos Fdez. Llamas <hello@sirikon.me>
2021-01-18 00:06:11 +0100

    New favicon

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ npm i
 npm start # For development
 npm run build # For production
 ```
+
+## Credits
+
+- Favicon comes from [OpenMoji.org](https://openmoji.org). [Unicode 2714 "Check mark"](https://openmoji.org/library/#emoji=2714) by Carlin MacKenzie.
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import m from "mithril"
 import Home from './views/home/Home'
 import TaskList from './views/tasklist/TaskList'
 
-var root = document.getElementById('app')
+const root = document.getElementById('app')
 m.route(root, '/', {
     '/': { view: () => m(Home) },
     '/:backend/:key': (vnode) => {
