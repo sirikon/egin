794689cdfb30b0900351984d90d50e50b412bd65
Carlos Fdez. Llamas <hello@sirikon.me>
2021-03-09 19:01:05 +0100

    Add sentry

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,17 @@ import m from "mithril"
 import Home from './views/home/Home'
 import TaskList from './views/tasklist/TaskList'
 
+import * as Sentry from "@sentry/browser";
+import { Integrations } from "@sentry/tracing";
+
+Sentry.init({
+    dsn: "https://b3f8579e83e242fa88ce024467a1ab12@o546900.ingest.sentry.io/5668801",
+    integrations: [new Integrations.BrowserTracing()],
+    tracesSampleRate: 1.0,
+});
+
+Sentry.captureException(new Error('Testing sentry'));
+
 const root = document.getElementById('app')
 root && m.route(root, '/', {
     '/': { view: () => m(Home) },
